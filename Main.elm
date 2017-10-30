66fde843f6c8740f80523908c5d49b4710e449e8
monslow <mikeo@clarityvoice.com>
2017-10-30 08:07:09 -0400

    Added comments to break up sections, added types for categories and items, added decoders for categories and items

--- Main.elm
+++ Main.elm
@@ -1,7 +1,111 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, h1, header, text, img)
+import Html exposing (Html, div, h1, header, img, text)
 import Html.Attributes exposing (class, src, width)
+import Json.Decode as Decode exposing (Decoder)
+import Json.Decode.Pipeline as Pipeline exposing (decode, optional, required)
+
+
+-- Model
+
+
+initialModel : Model
+initialModel =
+    {}
+
+
+type alias Model =
+    { categories : List Category
+    , items : List Item
+    }
+
+
+type alias Category =
+    { id : Int, label : String }
+
+
+type alias Item =
+    { id : Int
+    , title : String
+    , categoryId : Int
+    , imageUrl : String
+    , linkUrl : String
+    , description : String
+    , overlayColor : String
+    }
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "container" ]
+        [ div [ class "row" ]
+            [ div
+                [ class "col"
+                ]
+                [ text "Hello Workshop!" ]
+            ]
+        ]
+
+
+
+-- Update
+
+
+type Msg
+    = None
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+-- Subscriptions
+
+
+subscriptions =
+    \_ -> Sub.none
+
+
+
+-- JSON Decoding
+
+
+categoryDecoder : Decoder Category
+categoryDecoder =
+    decode Category
+        |> required "id" Decode.int
+        |> required "label" Decode.string
+
+
+itemDecoder : Decoder Item
+itemDecoder =
+    decode Item
+        |> required "id" Decode.int
+        |> required "title" Decode.string
+        |> required "categoryId" Decode.int
+        |> required "imageUrl" Decode.string
+        |> required "linkUrl" Decode.string
+        |> required "description" Decode.string
+        |> required "overlayColor" Decode.string
+
+
+
+-- Helpers
+
+
+(=>) : a -> b -> ( a, b )
+(=>) =
+    (,)
+
+
+
+-- Program Initialization
 
 
 main : Program Never Model Msg
@@ -14,37 +118,5 @@ main =
         }
 
 
-
 init =
     ( initialModel, Cmd.none )
-
-
-initialModel : Model
-initialModel =
-    {}
-
-
-type alias Model =
-    {}
-
-
-view : Model -> Html Msg
-view model =
-    div [ class "content" ]
-        [ img [ src "static/images/elm-detroit.png", class "logo" ] []
-        , header [] [ h1 [] [  text "Introduction to Elm" ] ]
-        , div [ class "intro-message" ] [ text "You are good to go!" ]
-        ]
-
-
-type Msg
-    = None
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
-
-
-subscriptions =
-    \_ -> Sub.none
